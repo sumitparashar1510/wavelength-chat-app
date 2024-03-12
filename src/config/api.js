07d940ddfe907414da1c259798693c109f9faa6a
sumitparashar1510 <sparashar.sp27@gmail.com>
2024-03-12 19:35:43 +0530

    testing backend

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,2 @@
-export const BASE_API_URL = "http://wavelength-backend-env.eba-rna3mwp3.ap-south-1.elasticbeanstalk.com";
+// export const BASE_API_URL = "http://wavelength-backend-env.eba-rna3mwp3.ap-south-1.elasticbeanstalk.com";
+export const BASE_API_URL = "http://localhost:5000";
